eaee48435d140a46660684eccc3eddc1773516f5
akpkick666 <s171928q@st.go.tuat.ac.jp>
2022-01-01 18:06:19 +0900

    詳細画面１

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,15 +7,12 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    /**
- * Post一覧を表示する
- * 
- * @param Post Postモデル
- * @return array Postモデルリスト
- */
- 
     public function index(Post $post){
         return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+    }
+ 
+    public function show(Post $post){
+        return view('posts/show')->with(['post' => $post]);
     }
     
 }

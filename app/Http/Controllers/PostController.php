1fbf644dbf73a27ae11e1b3062f42606de6b5fe0
akpkick666 <s171928q@st.go.tuat.ac.jp>
2022-01-06 19:25:30 +0900

    編集画面作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,10 +19,13 @@ class PostController extends Controller
         return view("posts/create");
     }
     
-    public function store(PostRequest $request, Post $post)
-{
+    public function store(PostRequest $request, Post $post){
         $input = $request['post'];
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
-}
+    }
+    
+    public function edit(Post $post){
+    return view('posts/edit')->with(['post' => $post]);
+    }
 }

78023ea19e5d2944395d32e5a2275b711d2da1c5
akpkick666 <s171928q@st.go.tuat.ac.jp>
2022-01-08 14:33:40 +0900

    編集画面入力チェック

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -17,11 +17,13 @@
             @method('PUT')
             <div class="title">
                 <h2>Title</h2>
-                <input type="text" name="post[title]" placeholder="タイトル" value="{{ $post->title }}"/>
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{old("post.title")?old("post.title"): $post->title }}"/>
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             <div class="body">
                 <h2>Body</h2>
-                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{ $post->body }}</textarea>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{ old("post.body")?old("post.body"): $post->body }}</textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
             <input type="submit" value="update"/>
         </form>

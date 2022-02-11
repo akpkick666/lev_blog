6c69dab0cdd0a69d4cdf22ea97b1ad6667b123d1
akpkick666 <s171928q@st.go.tuat.ac.jp>
2022-02-11 15:17:07 +0900

    key設定-guzzle導入

---
+++ database/seeds/Area_cityTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Area_cityTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+    }
+}

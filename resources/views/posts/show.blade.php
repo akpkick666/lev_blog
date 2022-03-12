03f35bcdbfe91b0728d771a4b08afb4f3692993a
akpkick666 <s171928q@st.go.tuat.ac.jp>
2022-03-12 18:23:21 +0900

    register画面ポップアップ追加

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -12,7 +12,38 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
     </head>
     <body>
-        @section('content')
+    @section('content')
+    
+        <section class="bg-light py-5">
+            <div class="container px-5 my-5 px-5">
+                <div class="text-center mb-5">
+                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-envelope"></i></div>
+                    <h2 class="fw-bolder">Get in touch</h2>
+                    <p class="lead mb-0">We'd love to hear from you</p>
+                </div>
+            
+                
+                <div class="row gx-5 justify-content-center">
+                    <div class="col-lg-6">
+                        
+                        
+                        
+                            
+                        
+                        
+                        
+                    </div>
+                </div>
+            </div>
+        </section>   
+        
+        
+       
+    
+        
+        
+        
+        
         <h1>Blog Name</h1>
         <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
         <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post" style="display:inline" onsubmit="return check();">
@@ -41,6 +72,6 @@
                 }
             }
         </script>
-        @endsection
+    @endsection
     </body>
 </html>
